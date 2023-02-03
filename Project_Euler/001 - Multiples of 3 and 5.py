7ec23e98ddce1ce1ef55219d32a1408cdb282a83
Demircan Özdemir <56583622+demircanozdemir@users.noreply.github.com>
2023-02-04 01:45:49 +0300

    Update 001 - Multiples of 3 and 5.py
    yazım düzeltmeleri

--- Project_Euler/001 - Multiples of 3 and 5.py
+++ Project_Euler/001 - Multiples of 3 and 5.py
@@ -1,7 +1,10 @@
-toplam=0
+toplam = 0
 for n in range(1,1000):
-    if(n%3==0):
-        toplam+=n
-    elif(n%5==0):
-        toplam+=n
+    
+    if(n%3 == 0):
+        toplam += n
+        
+    elif(n%5 == 0):
+        toplam += n
+        
 print(toplam)
